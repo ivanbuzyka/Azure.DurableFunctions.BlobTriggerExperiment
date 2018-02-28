5237538e6bea269ac2ef221f81757939cf1eb092
ivanbuzyka <ivanbuzyka@gmail.com>
2018-02-28 18:42:29 +0100

    Updated orchestrator to get blob name from client and download the blob using blob container binding. That is alternative solution to passing Stream from client to orchestrator which doesn't work

--- DurableEveDataAggregator.cs
+++ DurableEveDataAggregator.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace DurableEveDataAggregator
 {
@@ -10,20 +12,30 @@ namespace DurableEveDataAggregator
     {
         [StorageAccount("EveDataStore")]
         [FunctionName("DurableEveDataAggregationEntryPoint")]
-        public static async Task DurableEveDataAggregationEntryPoint([BlobTrigger("eve-data-temp/{name}")]Stream myBlob, string name, [OrchestrationClient] DurableOrchestrationClient starter,
+        public static async Task DurableEveDataAggregationEntryPoint([BlobTrigger("eve-data-temp/{name}")]CloudBlockBlob myBlob, string name, [OrchestrationClient] DurableOrchestrationClient starter,
             TraceWriter log)
         {
-            string instanceId = await starter.StartNewAsync("EveDurableOrchestrator", myBlob);
+            string instanceId = await starter.StartNewAsync("EveDurableOrchestrator", name);
 
             log.Info($"Started orchestration with ID = '{instanceId}'");
         }
 
         [FunctionName("EveDurableOrchestrator")]
-        public static async Task<int> Run([OrchestrationTrigger] DurableOrchestrationContext dataAggregatorContext, TraceWriter log)
+        public static async Task<int> Run([OrchestrationTrigger] DurableOrchestrationContext dataAggregatorContext, TraceWriter log, [Blob("eve-data-temp")] CloudBlobContainer container)
         {
-            var stream = dataAggregatorContext.GetInput<Stream>();
+            var blobName = dataAggregatorContext.GetInput<string>();
+            var blobReference = container.GetBlockBlobReference(blobName);
+            var dataStream = new MemoryStream();
 
-            log.Info($"Input stream length = '{stream.Length}'");
+            log.Info($"Downloading of the file '{blobName}'...");
+
+            await blobReference.DownloadToStreamAsync(dataStream);
+            
+            log.Info($"Downloading of the file '{blobName}'. Completed.");
+            log.Info($"The file size in stream: '{dataStream.Length}'");
+
+            //ToDo: do splitting the blob file to portions (should be sertializable collections) and fan out to activity function
+            //Probably that is not good idea to download a big blob here :)
 
             var results = new Task<int>[10];
 
